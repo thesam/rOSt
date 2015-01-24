7c28d299fd98b2fca8bab07853918f482be0e9ff
Samuel Moritz <samuel.moritz@gmail.com>
2015-01-24 23:53:27 +0100

    Working on box implementation, including asm stubs for functions needed by libcore

--- lib.asm
+++ lib.asm
@@ -1,9 +1,8 @@
 section .text
-global memset
-global asm_int_handler
 
 extern int_handler
 
+global memset
 memset:
     push ebp
     mov ebp,esp
@@ -34,8 +33,81 @@ memset_loop:
     pop ebp
     ret
 
+global asm_int_handler
 asm_int_handler:
     pushad
     call int_handler
     popad
     iret
+
+global __fixunssfdi
+__fixunssfdi:
+global __fixunsdfdi
+__fixunsdfdi:
+global __powisf2
+__powisf2:
+global __powidf2
+__powidf2:
+global __mulodi4
+__mulodi4:
+global __moddi3
+__moddi3:
+global __umoddi3
+__umoddi3:
+global __divdi3
+__divdi3:
+global __udivdi3
+__udivdi3:
+global trunc
+trunc:
+global truncf
+truncf:
+global fmod
+fmod:
+global fmodf
+fmodf:
+global pow
+pow:
+global powf
+powf:
+global floor
+floor:
+global floorf
+floorf:
+global log10
+log10:
+global log10f
+log10f:
+global memcpy
+memcpy:
+global memcmp
+memcmp:
+global log
+log:
+global log2
+log2:
+global exp
+exp:
+global exp2
+exp2:
+global exp2f
+exp2f:
+global fma
+fma:
+global fmaf
+fmaf:
+global round
+round:
+global roundf
+roundf:
+global ceil
+ceil:
+global ceilf
+ceilf:
+global log2f
+log2f:
+global logf
+logf:
+global expf
+expf:
+    jmp $
--- main.rs
+++ main.rs
@@ -2,6 +2,7 @@
 
 #![feature(asm)]
 #![feature(lang_items)]
+#![feature(box_syntax)]
 
 #[allow(unstable)]
 extern crate core;
@@ -15,6 +16,11 @@ mod console;
 mod asm;
 mod interrupt;
 
+use core::ptr::Unique;
+
+#[lang="owned_box"]
+pub struct Box<T>(Unique<T>);
+
 #[no_mangle]
 #[no_stack_check]
 pub fn main() {
@@ -24,6 +30,8 @@ pub fn main() {
     console.print_string("End world");
     console.print_char('A');
     console.print_int(1234567890);
+    let boxed = box 5;
+    console.print_int(*boxed);
 }
 
 // Stubs for functions needed to build as static lib.
@@ -31,4 +39,23 @@ pub fn main() {
 #[lang = "eh_personality"] extern fn eh_personality() {}
 #[lang = "panic_fmt"] fn panic_fmt() -> ! { loop{} }
 
+static mut heap:u8 = 0;
+
+#[lang="exchange_malloc"]
+unsafe fn allocate(size: usize, _align: usize) -> *mut u8 {
+//    let p = libc::malloc(size as libc::size_t) as *mut u8;
+
+//    // malloc failed
+//    if p as usize == 0 {
+//        abort();
+//    }
+//
+//    p
+    let p:*mut u8 = &mut heap;
+    return p;
+}
+#[lang="exchange_free"]
+unsafe fn deallocate(ptr: *mut u8, _size: usize, _align: usize) {
+//    libc::free(ptr as *mut libc::c_void)
+}
 
