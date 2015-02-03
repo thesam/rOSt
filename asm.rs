b8557bf9e186d28d393900ba18cf7f55d90101c7
Samuel Moritz <samuel.moritz@gmail.com>
2015-02-03 11:01:24 +0100

    A first step into reading PCI configuration

--- asm.rs
+++ asm.rs
@@ -20,3 +20,16 @@ pub fn inb(port:u16) -> u8 {
     }
     return value;
 }
+
+pub fn in32(port:u16) -> u32 {
+    let mut value:u32;
+    unsafe {
+        //TODO: Figure out the input/output constraints, is this correct?
+        asm!(
+        "in $1,$0"
+        : "={eax}"(value)
+        : "{dx}"(port)
+        )
+    }
+    return value;
+}
--- main.rs
+++ main.rs
@@ -26,12 +26,18 @@ pub struct Box<T>(Unique<T>);
 pub fn main() {
     let mut console = console::Console::init();
     console.clear_screen(Color::LightRed);
+
     console.print_string("Hello world");
-    console.print_string("End world");
+
     console.print_char('A');
-    //console.print_int(1234567890);
-    //let boxed = box 5;
-    //console.print_int(*boxed);
+    console.print_int(1234567890);
+
+    let boxed = box 5;
+    console.print_int(*boxed);
+
+    console.print_int(pci_config_read(0,0,0,0) as u32);
+
+    console.print_string("End world");
 }
 
 // Stubs for functions needed to build as static lib.
@@ -39,23 +45,32 @@ pub fn main() {
 #[lang = "eh_personality"] extern fn eh_personality() {}
 #[lang = "panic_fmt"] fn panic_fmt() -> ! { loop{} }
 
+// Dynamic allocation of memory
 static mut heap:u8 = 0;
 
 #[lang="exchange_malloc"]
 unsafe fn allocate(size: usize, _align: usize) -> *mut u8 {
-//    let p = libc::malloc(size as libc::size_t) as *mut u8;
-
-//    // malloc failed
-//    if p as usize == 0 {
-//        abort();
-//    }
-//
-//    p
+    //TODO: Implement support more for than 1 byte :)
     let p:*mut u8 = &mut heap;
     return p;
 }
 #[lang="exchange_free"]
 unsafe fn deallocate(ptr: *mut u8, _size: usize, _align: usize) {
-//    libc::free(ptr as *mut libc::c_void)
+    //TODO: Implement
 }
 
+// PCI, from OSDev wiki
+fn pci_config_read(bus: u8, slot: u8, func: u8, offset: u8) -> u16 {
+    let address:u32;
+    let lbus:u32 = bus as u32;
+    let lslot:u32 = slot as u32;
+    let lfunc:u32 = func as u32;
+    let tmp:u16;
+
+    address = ((lbus << 16) | (lslot << 11) |
+              (lfunc << 8) | (offset as u32 & 0xfc) | (0x80000000 as u32));
+
+    /* (offset & 2) * 8) = 0 will choose the first word of the 32 bits register */
+    tmp = ((asm::in32 (0xCFC) >> ((offset as u32 & 2) * 8)) & 0xffff) as u16;
+    return tmp;
+}
